a18282d6009835e90a112e2b2cffb30828c6e44b
Helin Taskesen <helintaskesen@gmail.com>
2026-03-13 19:16:08 -0400

    adding logo to tab bar

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "Lemontree Insights",
   description: "Data insights platform for food access partners",
+  icons: {
+    icon: "https://www.foodhelpline.org/_next/static/media/logo.b8e851d7.svg",
+  },
 };
 
 export default function RootLayout({ children }) {
